8af9df37c2fc41fc6c5f3b76ee7d212d3d92bca4
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-02 20:16:52 -0400

    Initial OAuth setup and passport google strategy config

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 
 // Require Express 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config.keys');
 
 // Configure app with port & routing 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+// Creates a new instance of passport google strategy
+// Need to pass keys & specify redirect route 
+passport.use(
+  new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+        console.log(accessToken);
+    }
+  )
+);
 
 // Dynamic Port Binding 
 const PORT = process.env.PORT || 5000
