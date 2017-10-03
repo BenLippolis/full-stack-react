976f7c79bd1917c28aa31a47a2ab77b775868af7
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-02 20:59:47 -0400

    Finalize google strategy config and return routes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config.keys');
+const keys = require('./config/keys');
 
 // Configure app with port & routing 
 const app = express();
@@ -18,11 +18,27 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, 
-    accessToken => {
-        console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refreah token', refreshToken);
+        console.log('profile', profile);
     }
   )
 );
+
+// Specify route for entering passport auth flow 
+// Google Strategy has an intermal identifier of 'google' which we call below
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+);
+
+// Route to handle google redirect + code
+// Passport will detect the code inside the URL 
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 // Dynamic Port Binding 
 const PORT = process.env.PORT || 5000
