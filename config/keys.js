ee46ce1ec2035ad8a5b83e106473b5f52df4a5cf
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-04 15:31:45 -0400

    TS pt.4

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // When code is deployed to Heroku we'll be able to tell what envt we're in 
-if (process.env.NODE_ENV = 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     module.exports = require('./dev');
