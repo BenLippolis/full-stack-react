6d9a1f4fa57ee40a0268785b7b4b52c60e1ff940
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-03 22:27:58 -0400

    Refactor keys for production

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+// When code is deployed to Heroku we'll be able to tell what envt we're in 
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod');
+} else {
+    module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// Production environment keys
+// Set env variables for Heroku deploy
+module.exports = {
+    googleClientID: 'process.env.GOOGLE_CLIENT_ID',
+    googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
+    mongoURI: 'process.env.MONGO_URI',
+    cookieKey: 'process.env.COOKIE_KEY'
+};
