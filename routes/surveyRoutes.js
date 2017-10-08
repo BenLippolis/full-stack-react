93e125937da07af3e324346f4a234c14ed3a6ce4
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-08 15:42:12 -0400

    Create mailer, email template, and finalize survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body;
+        // Create new survey 
         // Note ES6 syntax with just title in place of title: title
         const survey = new Survey({
             title,
@@ -16,5 +19,8 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        // Send an email
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -5,5 +5,59 @@ const keys = require('../config/keys');
 
 // helper.Mail adds built in functionality from React to Mailer class 
 class Mailer extends helper.Mail {
-    
+    constructor({ subject, recipients }, content) { 
+        super();
+
+        this.sgApi = sendgrid(keys.sendGridKey);
+        // sendgrid specific
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients); 
+
+        // addContent is built in function from Mail class
+        this.addContent(this.body);
+        // Enable click tracking inside of email
+        this.addClickTracking();
+        // Add the recipients to Mailer 
+        this.addRecipients();
+    }
+
+    // Helper 
+    formatAddresses(recipients) {
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    // Helper
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking(true, true);
+
+        trackSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    // Helper
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
+
+    async send() {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: this.toJSON()
+        });
+        // Send off to sendgrid API 
+        const response = this.sgApi.API(request);
+        return response;
+    }
 }
+
+module.exports = Mailer;
