80ed5667b702d100b0e0b2008b368442e3c746c7
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-09 23:51:49 -0400

    Finalize survey form & add validations

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -34,7 +34,7 @@ class App extends Component {
         return (
             <div className="container">
                 <BrowserRouter>
-                    <div>
+                    <div className="container">
                         <Header />
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/surveys" component={Dashboard} />
--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,15 @@
 // label and text input 
 import React from 'react';
 
-export default ({ input, label }) => {
+// touched determines if an input field has been clicked yet 
+export default ({ input, label, meta: { error, touched } }) => {
     return (
         <div>
            <label>{label}</label> 
-           <input {...input} />
+           <input {...input} style={{ marginBottom: '5px'}}/>
+           <div className="red-text" style={{ marginBottom: '20px'}}>
+            {touched && error}
+           </div>
         </div>
     );
 };
