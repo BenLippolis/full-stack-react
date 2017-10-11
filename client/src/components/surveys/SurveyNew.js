02dffb52b6a15f2357f41acc219df4429f7d8004
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-11 00:07:39 -0400

    Add initial survey form review functionality

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,12 +1,24 @@
 // Survey New shows survey form 
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+    // Component level state example
+    state = { showFormReview: false };
+
+    renderContent() {
+        if (this.state.showFormReview) {
+            return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false})}/>;
+        }
+
+        return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
+    }
+    
     render() {
         return ( 
             <div>
-                <SurveyForm/>
+                {this.renderContent()}
             </div>
         );
     }
