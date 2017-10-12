bef012fba9971ed9c5ea7ff3b8f6a9530a6ce6c6
Benjamin Lippolis <lippolis.b@husky.neu.edu>
2017-10-12 16:32:27 -0400

    Added fields to form reveiw view and placed fields in a separate file

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { label: 'Survey Title', name: 'title' },
-    { label: 'Subject Line', name: 'subject' },
-    { label: 'Email Body', name: 'body' },
-    { label: 'Recipient List', name: 'emails' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return (
                 <Field 
                     key={name} 
@@ -52,7 +46,7 @@ function validate(values) {
     errors.emails = validateEmails(values.emails || '');
     
 
-    _.each(FIELDS, ({ name }) => {
+    _.each(formFields, ({ name }) => {
         if(!values[name]) {
             errors[name] = 'You must provide a value for this field.'
         }
@@ -64,6 +58,9 @@ function validate(values) {
 
 export default reduxForm({
     validate,
-    // To customize how the form behaves 
-    form: 'surveyForm'
+    // Name tells redux form how to successfully manage submitted values
+    // Giving this name to other forms allows them to share values 
+    form: 'surveyForm',
+    // Prevents form values from getting dumped when component is not rendered
+    destroyOnUnmount: false
 })(SurveyForm);
